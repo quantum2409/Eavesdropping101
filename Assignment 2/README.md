17710cd42a39b43fd8bd11a6774345615f51eeca
Pradnya2004 <131396808+Pradnya2004@users.noreply.github.com>
2023-06-15 22:11:18 +0530

    Add files via upload

--- Assignment 2/README.md
+++ Assignment 2/README.md
@@ -1,16 +1,1 @@
-# Magic is everywhere
-
-Well... You cracked the code and the time machine is off to its destination... 4 September 1998, Menlo Park, California, United States, the foundation of Google... You start going back.... from 2023 to when noone had heard about corona to 2010s to 2000s to 1990s... wait... the machine doesnt stop... it keeps going back in time... you are out of luck.... the machine crashes in 1690s... in the land of witches... Thats right... Salem! 
-<br></br>( [More on Salem Here](https://www.smithsonianmag.com/history/a-brief-history-of-the-salem-witch-trials-175162489/#:~:text=The%20Salem%20witch%20trials%20occurred,accused%20and%20compensated%20their%20families.). Note that this is not important for the assignment. Just for the sake of interest. )<br></br>
-
-You crash in a cave. There is are scriptures on the wall. They read of a story, a story of an evil wizard in this land of wiches. His name was Jaffar. This cave was cursed to be a vantage point for all of time by him. He wanted all the "time energy" for himself, so he would trap the time travellers' souls and extract all the "time energy" from their machines and body. The last person that came into this cave was named Abhi. He came for the vengence for his father and succesfully fought Jaffar out of existence but had to pay with his soul being trapped in a hole inside the cave. The cave remained the vantage point for all of time travel. This soul has all the knowledge and resources you need to escape from this place. So now you need to free this soul to free yourself. You start looking for a hole and finally find one. You hear this voive coming out of it: <br></br>
-
-https://github.com/quantum2409/Eavesdropping101/assets/98262532/6f8598c9-d769-4d45-b8ba-64e3cc9e3f7a
-
-You, as an occasional piano player and a music lover identify the notes as D, E, B, A, C being played repititively. You ask the soul "how do I free you?" and a message appear beneath the hole.
-
-```
-qmnjvsa nv wewc flct vprj tj tvvplvl fv xja vqildhc xmlnvc nacyclpa fc gyt vfvw. fv wgqyp, pqq pqcs y wsq rx qmnjvafy cgv tlvhf cw tyl aeuq fv xja tkbv cqnsqs. lhf avawnc cv eas fuqb qvq tc yllrqr xxwa cfy. psdc uqf avrqc gefq pyat trac xwv taa wwd dv eas flcbq. vd trawm vupq quw x decgqcwt, yq yafl vlqs yqklhq! snafq vml lhvqpawr nqg_vfusr_ec_wawy qp fn wgawdgf.
-```
-
-The last words you hear are "whisper the password and i'll be free" and then... The walls of the cave start moving in... They are in to crush you... You need to free the soul before 7th June 2023 EOD or you will be crushed to death. ALL THE BEST.
+BREAKER OF THIS CODE WILL BE BLESSED BY THE SQUEAKY SPIRIT RESIDING IN THE HOLE GO AHEAD, AND FIND A WAY OF BREAKING THE SPELL ON HIM CAST BY THE EVIL JAFFAR. THE SPIRIT OF THE CAVE MAN IS ALWAYS WITH YOU. FIND THE MAGIC WAND THAT WILL LET YOU OUT OF THE CAVES. IT WOULD MAKE YOU A MAGICIAN, NO LESS THAN JAFFAR! SPEAK THE PASSWORD THE_MAGIC_OF_WAND TO GO THRHGTO.U
---
+++ Assignment 2/main.cpp
@@ -0,0 +1,90 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    string encryptedText = "qmnjvsa nv wewc flct vprj tj tvvplvl fv xja vqildhc "
+                          "xmlnvc nacyclpa fc gyt vfvw. fv wgqyp, pqq pqcs y wsq "
+                          "rx qmnjvafy cgv tlvhf cw tyl aeuq fv xja tkbv cqnsqs. "
+                          "lhf avawnc cv eas fuqb qvq tc yllrqr xxwa cfy. psdc uqf "
+                          "avrqc gefq pyat trac xwv taa wwd dv eas flcbq. vd trawm "
+                          "vupq quw x decgqcwt, yq yafl vlqs yqklhq! snafq vml "
+                          "lhvqpawr nqg_vfusr_ec_wawy qp fn wgawdgf.";
+
+    
+    string decryptedText;
+    int index = 0;
+    while (index < encryptedText.length()) {
+        char characters[5];
+        string interstitials[5];
+        for (int curIndex = 0; curIndex < 5; curIndex++) {
+            if (isalpha(encryptedText[index])) {
+                characters[curIndex] = encryptedText[index];
+                // Take all the non-letters with it also
+                while (!isalpha(encryptedText[index + 1]) && index < encryptedText.length() - 1)
+                    interstitials[curIndex] += encryptedText[++index];
+            }
+            index++;
+        }
+        // Concatenate in new order
+        decryptedText += characters[3] + interstitials[0] + characters[2] + interstitials[1] +
+                        characters[4] + interstitials[2] + characters[0] + interstitials[3] + characters[1] + interstitials[4];
+    }
+
+    encryptedText = decryptedText;
+    string substitutionRules[] = { "Ow", "Aq", "Ta", "Sl", "Ph", "Ev", "Hf", "Gg", "Ud", "Rn", "Km", "Wr", "Dp", "Ic", "Ny", "Bj",
+                                   "Fs", "Ce", "Lt", "Yx", "Qi", "Mu", "Jk", "Vb" };
+
+    for (string rule : substitutionRules) {
+        size_t pos = encryptedText.find(rule[1]);
+        while (pos != string::npos) {
+            encryptedText.replace(pos, 1, string(1, rule[0]));
+            pos = encryptedText.find(rule[1], pos + 1);
+        }
+    }
+
+    
+    int p = 0;
+    bool afterFullStop = false;
+    bool replaceSpaces = false;
+    for (char c : encryptedText) {
+        if (!isalpha(c)) {
+            if (afterFullStop) {
+                cout << c;
+            } else {
+                if (c == '.') {
+                    afterFullStop = true;
+                    replaceSpaces = true;
+                } else if (c == '_') {
+                    cout << ' ';
+                } else {
+                    if (replaceSpaces) {
+                        cout << " " << c;
+                        replaceSpaces = false;
+                    } else {
+                        cout << c;
+                    }
+                }
+            }
+            continue;
+        }
+        cout << c;
+        p++;
+        if (p == 5) {
+            p = 0;
+            
+        }
+    }
+
+    return 0;
+}
